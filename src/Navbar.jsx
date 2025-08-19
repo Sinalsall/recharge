9f3b984650cdc043f05530c173fae55b76a5898a
TheoLN <theoliviusnatael@gmail.com>
2025-08-19 19:34:44 +0700

    sebagian trivia

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function Navbar(){
     return (<nav className= "nav"> 
-        <img src="src\assets\logo recharge.png" width="150" height="auto"/>
+        <img src="src\assets\logo recharge.png" width="80" height="auto"/>
         <ul>
             <li> <CustomLink to="/">Home</CustomLink> </li>
             <li> <CustomLink to ="/info">Basic Information</CustomLink> </li>
-            <li> <CustomLink to="/education">Education</CustomLink> </li>
+            <li> <CustomLink to="/education">Recharge Trivia</CustomLink> </li>
             <li> <CustomLink to="/map">Interactive map</CustomLink></li>
             <li> <CustomLink to="/sumur">Data Real Time</CustomLink> </li> 
             <li> <CustomLink to="/calculator">Calculator</CustomLink></li>
--- src/page/Education.jsx
+++ src/page/Education.jsx
@@ -1,7 +1,25 @@
 export default function Education(){
     return(
         <>
-        <h1>Materi Edukasi</h1>
+        <h1>Recharge Trivia</h1>
+        <h2>The 101 You Need To Know</h2>
+        <ul className="trivia" >
+            <li>
+                Groundwater 101 - The Invisible Lake Under Your Feet
+            </li>
+            <li>
+                Aquifers 101 — Sponges vs. Bricks Underground
+            </li>
+            <li>
+                Recharge 101 — Refilling the Underground Bank
+            </li>
+            <li>
+                Injection Wells 101 — A Straw That Pushes Water Down (Safely)
+            </li>
+            <li>
+                Water Quality 101 — Only Good Water In = Good Water Out
+            </li>
+        </ul>
         </>
     )
 }
