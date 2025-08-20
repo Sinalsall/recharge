0cee57e73ca6b50cf127fbb66a292f855436bfa8
TheoLN <theoliviusnatael@gmail.com>
2025-08-20 10:13:36 +0700

    add 5 other map

--- src/page/InteractiveMap.jsx
+++ src/page/InteractiveMap.jsx
@@ -3,14 +3,34 @@ import "leaflet/dist/leaflet.css";
 
 
 function InteractiveMap() {
-  
+  const [activeIframe,setActiveIframe] =useState(0);
+  const iframeUrls= [
+    "qgis2web_2025_08_19-17_02_26_291282\\index.html",
+    "Web Map\\Curah Hujan\\qgis2web_2025_08_20-00_40_00_086360\\index.html",
+    "Web Map\\Drain Density\\qgis2web_2025_08_20-00_59_25_610735\\index.html",
+    "Web Map\\Jenis Tanah\\qgis2web_2025_08_20-01_02_49_900079\\index.html",
+    "Web Map\\LST\\qgis2web_2025_08_20-00_53_09_223397\\index.html",
+    "Web Map\\Overlay\\qgis2web_2025_08_19-23_21_03_822955\\index.html"
+  ];
 
   return (
+    <>
+    <div>
+      <ul className="map-button-holder">
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(0)}>Clean Water Demand Index</button></li>
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(1)}>Curah Hujan</button></li>
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(2)}>Drain Density</button></li>
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(3)}>Jenis Tanah</button></li>
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(4)}>LST</button></li>
+      <li><button className="map-toggle" onClick={()=>setActiveIframe(5)}>Injection Suitability Index</button></li>
+      </ul>
+    </div>
     <iframe
     src="qgis2web_2025_08_19-17_02_26_291282\index.html"
     className ="interactive-map"
-    title="first map"
+    title="Interactive Map"
     />
+    </>
   );
 }
 
